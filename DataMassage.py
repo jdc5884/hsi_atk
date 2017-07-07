ab46d56562ba1464886e54ba2437eb9269364317
David Ruddell <dlruddell@gmail.com>
2017-07-07 13:51:00 -0400

    Converted some of the data's label string values to integers for further analysis

---
+++ DataMassage.py
@@ -0,0 +1,38 @@
+__author__ = "David Ruddell"
+# contact: dr1236@uncw.edu, dlruddell@gmail.com
+
+"""
+File to convert string values to integers for regression and other
+analysis methods.
+"""
+
+import csv
+
+old_data = open('headers3mgperml.csv', 'r')
+
+new_data = open('massaged_data.csv', 'w')
+
+csv_reader = csv.reader(old_data, delimiter=',')
+
+csv_writer = csv.writer(new_data, delimiter=',')
+
+for row in csv_reader:
+    n_count = 0
+    row.pop(14)
+
+    for n in row:
+        if n == 'B73' or n == 'NORMAL' or n == 'CONTROL':
+            row[n_count] = 0
+        elif n == 'CML103' or n == 'LOW' or n == 'HIGH' or n == 'PAC':
+            row[n_count] = 1
+        elif n == 'PACGA':
+            row[n_count] = 2
+        elif n == 'UCN':
+            row[n_count] = 3
+        elif n == 'PCZ':
+            row[n_count] = 4
+        elif n == 'GA':
+            row[n_count] = 5
+        n_count += 1
+    csv_writer.writerow(row)
+
