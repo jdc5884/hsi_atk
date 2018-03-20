9aa728dd4320ead0fca0abf55166dfb83f6faca2
David Ruddell <dlruddell@gmail.com>
2018-03-20 17:58:31 -0400

    Added more functions making generator more robust. Can now create multiple shapes in an array, and made data more modular in general.

--- HSI_ATK/Generators/simple_gen.py
+++ HSI_ATK/Generators/simple_gen.py
@@ -6,10 +6,6 @@ import skimage.draw as draw
 import pprint
 
 
-# Size for each dimension
-n = 50
-m = 50
-
 r1 = 4-sqrt(10)
 r2 = 4-sqrt(5)
 r3 = 4-sqrt(2)
@@ -29,6 +25,8 @@ sim_data = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 # data_skeleton = np.array(data_skeleton)
 
+# TODO: generator for collections of shapes.
+
 # class cone():
 #     def __init__(self, radius, height, step):
 #         self.r, r = radius
@@ -36,31 +34,76 @@ sim_data = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
 #         self.s, s = step
 #         self.z_vals = self.enum(r,s,h)
 
-# TODO: shape gen with input array
-def enum_cone(shape, xc, yc, r, h):
+def square1d(shape, val=0):
+    im = [val]*shape
+    return im
+
+def square2d(shape, val=0):
+    im = [[val]*shape[0] for i in range(shape[1])]
+    return im
+
+def square3d(shape, val=0):
+    im = [[[val]*shape[0] for i in range(shape[1])] for j in range(shape[2])]
+    return im
+
+def cone_lin_func(r, h, x, y, xC, yC, hmod, rd):
+    return round(-(h/r)*np.sqrt(((x-xC)**2+(y-yC)**2)/(r/h)**2)+h+hmod, rd)
+
+def enum_cone(shape, xc, yc, r, h, hmod=0, rd=4):
     """
     generate 2d array of z values for a conic
     """
     z = [[0 for i in range(shape[0]+1)] for j in range(shape[1]+1)]
-    z[xc][yc] = h
+    # z[xc][yc] = h
     m = h/r
     for i in range(0,shape[0]+1):
         for j in range(0,shape[1]+1):
-            zd = -m*np.sqrt(((i-xc)**2+(j-yc)**2)/(r/h**2))+h
+            zd = cone_lin_func(r, h, i, j, xc, yc, hmod, rd)
             if zd > 0:
-                z[i][j] = zd
+                z[i][j] += zd
             else:
                 pass
     return z
 
-cone1 = np.array(enum_cone((25, 25), 11, 11, 5, 5))
+def cone_gen(im_array, xc, yc, r, h, hmod=0):
+    xL = int(np.floor(xc - r))
+    yB = int(np.floor(yc - r))
+    xR = int(np.ceil(xc + r))
+    yT = int(np.ceil(yc + r))
+    for i in range(xL, xR):
+        for j in range(yB, yT):
+            zd = cone_lin_func(r, h, i, j, xc, yc, hmod, 4)
+            if zd > 0:
+                im_array[i][j] = zd
+            else:
+                pass
+    return im_array
 
-cone2 = np.array(enum_cone((500,640), 55, 200, 30, 50))
 
+print(cone_lin_func(10,10,35,35,35.5,35.5,0,4))
 
-ellip0 = draw.ellipsoid(30, 30, 240, spacing=(1.0, 1.0, 1.0), levelset=True)
-ellip0s = draw.ellipsoid_stats(30, 30, 240)
-
-ell = np.array(ellip0)
-ell_s = ell.shape
-ell = ell.reshape(ell_s[0]*ell_s[1], -1)
+# sq1 = np.array(square1d(200))
+sq2 = square2d((200,200))
+# sq3 = np.array(square3d((200,200,10)))
+sq2 = np.array(cone_gen(sq2, 35.5, 35.5, 10, 10))
+sq2 = np.array(cone_gen(sq2, 50, 50, 11, 12))
+sq2 = np.array(cone_gen(sq2, 100, 100, 11, 12))
+sq2 = np.array(cone_gen(sq2, 20, 50, 10, 15))
+### Testing generation
+# cone1 = np.array(enum_cone((25, 25), 11, 11, 5, 5))
+#
+# cone2 = np.array(enum_cone((500,640), 55, 200, 50, 50))
+#
+# cone3 = np.array(enum_cone((400,400), 35.5, 35.5, 10, 10))
+#
+# cone4 = np.array(enum_cone((400,400), 35.2, 35.7, 10, 10))
+#
+# cone5 = np.array(enum_cone(()))
+#
+#
+# ellip0 = draw.ellipsoid(30, 30, 240, spacing=(1.0, 1.0, 1.0), levelset=True)
+# ellip0s = draw.ellipsoid_stats(30, 30, 240)
+#
+# ell = np.array(ellip0)
+# ell_s = ell.shape
+# ell = ell.reshape(ell_s[0]*ell_s[1], -1)
