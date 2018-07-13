687aaebc0d075f51b53fcf606fcdc785f759d74d
David Ruddell <dlruddell@gmail.com>
2018-07-13 15:33:21 -0400

    Added output for label array from compose() function, altered some function values for label and structure generation, testing clustering on generated images

--- hsi_atk/pentiga_scratch.py
+++ hsi_atk/pentiga_scratch.py
@@ -7,10 +7,14 @@ cents = [(0, 0), (-3, -4), (2, 5)]
 
 
 def nfunc(x):
-    return 100.*np.sin(.02*x - 1)*np.cos(.05*x-5) + 600 + x
+    return 100. * np.sin(.02 * x - 1)*np.cos(.05 * x - 5) + 600 + x
 
 
-def npentiga(name, base_sma, sub_smas):
+# def sub_0(x):
+#     return
+
+
+def npentiga(name, base_sma, sub_smas, sub_dists):
     # pent0 = basic_gen("pent0", (240, 20, 30), smas, cents)
     import time
     time0 = time.time()
@@ -24,31 +28,37 @@ def npentiga(name, base_sma, sub_smas):
     i = 0
     for sma in sub_smas:
         sub_name = name + "_" + str(i)
-        pent.gen_sub_structure(sma, name=sub_name, stats=True)
+        pent.gen_sub_structure(sma, name=sub_name, dist_center=sub_dists[i], stats=True)
 
         pent.sub_structures[sub_name].scale_structure(-25, 200)  # scale brightness of sub str
         pent.sub_structures[sub_name].add_func_bandwise(nfunc, (0, 240))  # scale sub
         i += 1
 
-    n_img = pent.compose()  # create composed image of base and sub structures
+    n_img, label_arr = pent.compose(return_labels=True)  # create composed image of base and sub structures
 
     time1 = time.time()
     print(time1-time0)
 
-    return pent, n_img
+    return pent, n_img, label_arr
 
 
 sub_smas = [
-    (15, 13, 240),
-    (12, 11, 240),
+    (30, 33, 240),
     (20, 25, 240),
+    (10, 11, 240),
 ]
 
-pent0, n_img0 = npentiga("pent", (35, 37, 240), sub_smas)
+dists = [
+    (3, -3),
+    (2, 5),
+    (0, 0),
+]
+
+pent0, n_img0, label_arr = npentiga("pent", (35, 37, 240), sub_smas, dists)
 
 
 def wt_func(x):
-    return x ** 0.75
+    return (x ** 0.75) / 1000
 
 
 str_wts = {'pent_0': .01, 'pent_1': .02, 'pent_2': .5}
@@ -57,23 +67,23 @@ pent0.gen_wt_label(wt_func, str_wts.keys(), str_wts)
 
 
 def lp_func(x):
-    return x ** 0.75
+    return x ** 0.95
 
 
 def palm_func(x):
-    return x ** 0.25
+    return x ** 0.45
 
 
 def lino_func(x):
-    return x ** 0.5
+    return x ** 0.65
 
 
 def olei_func(x):
-    return x ** 0.5
+    return x ** 0.55
 
 
 def stea_func(x):
-    return x ** 0.5
+    return x ** 0.185
 
 
 lp_wts = {'pent_0': {'lp': 0.2, 'palm': 0.05, 'lino': 0.04, 'olei': 0.01, 'stea': 0.15},
@@ -81,3 +91,10 @@ lp_wts = {'pent_0': {'lp': 0.2, 'palm': 0.05, 'lino': 0.04, 'olei': 0.01, 'stea'
           'pent_2': {'lp': 0.5, 'palm': 0.5, 'lino': 0.2, 'olei': 0.1, 'stea': 0.075}}
 
 pent0.gen_lp_labels(lp_func, palm_func, lino_func, olei_func, stea_func, 1, str_wts.keys(), lp_wts)
+
+from skhyper.cluster import KMeans
+from skhyper.process import Process
+
+X = Process(n_img0)
+mdl = KMeans(5)
+mdl.fit(X)
