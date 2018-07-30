41e1ca3cf8758ef5bf82edbaec883fa768208386
David Ruddell <dlruddell@gmail.com>
2018-07-30 14:17:39 -0400

    Modified clustering to deal with memory issues of skhyper clustering. Added grabbing of statistics of classes.

--- hsi_atk/real_cl_stats.py
+++ hsi_atk/real_cl_stats.py
@@ -3,9 +3,10 @@ import numpy as np
 import pandas as pd
 
 from hsi_atk.dataset import open_hsi_bil
+from sklearn.svm import SVR, SVC
 from skhyper.cluster import KMeans
 from skhyper.process import Process
-from skhyper.svm import SVC
+# from skhyper.svm import SVC
 from skimage.filters import threshold_otsu
 
 
@@ -49,6 +50,7 @@ def get_lbld_img_paths(df):
 
 def build_4d_img(paths):
     images = []
+
     count = 0
     rmin = 500
     rmax = 0
@@ -66,13 +68,15 @@ def build_4d_img(paths):
         if ncmax > cmax:
             cmax = ncmax
         images.append(img)
+
         count += 1
-        # if count == 5:
-        #     break
+        if count == 1:
+            break
         print("Processed ", count, " image(s)...")
 
     images = np.array(images)
     images = images.swapaxes(0, 3).swapaxes(0, 2).swapaxes(0, 1)
+
 
     return images[rmin:rmax, cmin:cmax, :, :]
 
@@ -107,45 +111,96 @@ def load_cl(path):
     hsi = filter_kernelspace(hsi)
     print('Processing hsi...')
     X = Process(hsi)
-    kmeans = KMeans(8, copy_x=False)
+    kmeans = KMeans(9, copy_x=False)
     print('Begin Clustering...')
     kmeans.fit(X)
+    labels = kmeans.labels_
 
-    return kmeans.labels_
+    img_cl_stats = {}
+    uniq, unic = np.unique(labels, return_counts=True)
+    print(uniq, "\n\n", unic)
+    means = []
+    meds = []
+    vars = []
+    stds = []
+    mins = []
+    maxs = []
+    counts = []
+
+    for i in uniq:
+        cl = "class_" + str(i)
+        # print(cl)
+        rr0, cc0 = np.where(labels == i)
+        nhsi = hsi[rr0, cc0, :]
+        # print(nhsi.shape)
+        coun = nhsi.shape[0]
+
+        mean = np.mean(nhsi, 0)
+        med = np.median(nhsi, 0)
+        var = np.mean(nhsi, 0)
+        std = np.std(nhsi, 0)
+        min_ = nhsi.min(axis=0)
+        max_ = nhsi.max(axis=0)
+
+        means.append(mean)
+        meds.append(med)
+        vars.append(var)
+        stds.append(std)
+        mins.append(min_)
+        maxs.append(max_)
+        counts.append(coun)
+
+    img_cl_stats['means'] = means
+    img_cl_stats['meds'] = meds
+    img_cl_stats['vars'] = vars
+    img_cl_stats['stds'] = stds
+    img_cl_stats['mins'] = mins
+    img_cl_stats['maxs'] = maxs
+    img_cl_stats['counts'] = counts
+
+    return img_cl_stats
 
 
-def getstats(path, geno):
-    # hsi_info = []
-    svc = SVC(kernel='linear', degree=5)
-    img_dt = {}
-    images = os.listdir(path)
-    count = 0
-    for img in images:
-        print(count)
-
-        if img.endswith('.bil'):
-            lbls = load_cl(path + img)
-            uni_v, uni_c = np.unique(lbls, return_counts=True)
-
-            if count == 0:
-                svc.fit(img, lbls)
-
-
-            # img_dt['mdl'] = km
-            # hsi_info.append(img_dt)
-
-            img_dt[geno+img] = lbls
-            count += 1
-        # if count == 5:
-        #     break
-
-    # return hsi_info
-    return img_dt
+# def getstats(path, geno):
+#     # hsi_info = []
+#     svc = SVC(kernel='linear', degree=5)
+#     img_dt = {}
+#     images = os.listdir(path)
+#     count = 0
+#     for img in images:
+#         print(count)
+#
+#         if img.endswith('.bil'):
+#             lbls = load_cl(path + img)
+#             uni_v, uni_c = np.unique(lbls, return_counts=True)
+#
+#             if count == 0:
+#                 svc.fit(img, lbls)
+#
+#
+#             # img_dt['mdl'] = km
+#             # hsi_info.append(img_dt)
+#
+#             img_dt[geno+img] = lbls
+#             count += 1
+#         # if count == 5:
+#         #     break
+#
+#     # return hsi_info
+#     return img_dt
 
 
 paths = get_lbld_img_paths(labeled_data)
-img_4d = build_4d_img(paths)
-print(img_4d.shape)
-# X = Process(img_4d)
-# mdl = KMeans(9, n_jobs=1)
-# mdl.fit(X)
+img_stats = {}
+# for path in paths:
+#     img_cl_st = load_cl(path)
+#     img_stats[path] = img_cl_st
+
+from sklearn.cluster import KMeans as BKMeans
+
+img_cl_st = load_cl("../Data/32.control.bil")
+
+
+# img_4d = build_4d_img(paths)
+# mdl0 = KMeans(9, n_jobs=1)
+# mdl0.fit(X0)
