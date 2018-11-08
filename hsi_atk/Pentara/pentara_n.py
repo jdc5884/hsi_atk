394ace448233f6f2e38b73d50c0a5d5d2f2f7611
David Ruddell <dlruddell@gmail.com>
2018-11-08 13:19:49 -0500

    Refactoring pentara to manage pentiga trials

--- hsi_atk/Pentara/pentara_n.py
+++ hsi_atk/Pentara/pentara_n.py
@@ -1,16 +1,25 @@
+
+
 class Pentara_n:
 
-    def __init__(self, shape, shape_pars, scale_aoi, scale_fn, name=None):
+    def __init__(self, name, shape, shape_pars, scale_aoi, scale_fn, label_fns, label_pars):
+        base_name = name
         base_shape = shape
         base_params = shape_pars
-        scale_aoi = scale_aoi
-        scale_fn = scale_fn
+        sc_aoi = scale_aoi
+        sc_fn = scale_fn
+        lbl_fns = label_fns
+        lbl_pars = label_pars
 
-        name = name
+    def gen_batch(self, n, alpha=0.01, beta=0.1, gamma=0.1, use_std=True, use_mean=True):
+        '''Generates batch of pentiga objects
+        :param n: int - number of pentigas
+        :param alpha: float - range to stretch wavelength parameters
+        :param beta: float - range to stretch shape of object
+        :param gamma: float - range to correlation coef
+        :param use_std: bool - use standard deviation of pixels in label generation
+        :param use_mean: bool - use mean of pixels in label generation
+        :return: dict of pentiga objects
+        '''
 
-        sub_pents = {}
-        labels = {}
-        label_fns = {}
-
-    # def add_sub(self, pentara_n):
-    #     self.sub_pents
+        return None
