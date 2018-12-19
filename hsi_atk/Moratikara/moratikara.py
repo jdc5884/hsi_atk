8dc3928e677a5723749f9b393d8fb03c1e40df53
David Ruddell <dlruddell@gmail.com>
2018-12-19 12:19:25 -0500

    fixed unexpected EOF

--- hsi_atk/Moratikara/moratikara.py
+++ hsi_atk/Moratikara/moratikara.py
@@ -1,14 +1,7 @@
 import numpy as np
-from skimage.draw import ellipsoid
 
 
 class Moratikara:
 
-    def __init__(self, sma):
-        """
-
-        :param sma: tuple of 3 ints - semi-major axes of ellipsoid representing
-                    3d structure of kernel
-
-        """
-
+    def __init__(self):
+        pass
