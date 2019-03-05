0f662640e3ead2476092091c8b299593ca134397
David Ruddell <dlruddell@gmail.com>
2019-03-05 18:37:15 -0500

    Added features to kernelhsidset and methods in file

--- hsi_atk/utils/dataset.py
+++ hsi_atk/utils/dataset.py
@@ -61,7 +61,8 @@ class Dataset():
         :param group_path: string - path through root -> group -> subgroups -> etc
         :return: None
         """
-        if "w" in self.get_mode():
+        mode = self.get_mode()
+        if "w" in mode or "a" in mode:
             hf = self.get_hf()
             group_name = group_path + group_name
             hf.create_group(group_name)
@@ -77,7 +78,8 @@ class Dataset():
         :param autochunk: boolean - whether or not to allow h5 to auto chunk. Helpful for larger dataset access performance
         :return: None
         """
-        if "w" in self.get_mode():
+        mode = self.get_mode()
+        if "w" in mode or "a" in mode:
             hf = self.get_hf()
             group = hf[group_path]
             group.create_dataset(dataset_name, data=data, chunks=autochunk)
@@ -92,31 +94,65 @@ class HSIDataset(Dataset):
 
 
 class KernelHSIDset(Dataset):
+    """Dataset class specified for dealing with HSI kernels in Stapleton lab dataset"""
 
-    def __init__(self, h5_file, genotypes=('B73','CML103'), mode='a'):
+    def __init__(self, h5_file, mode='a'):
         super().__init__(h5_file=h5_file, mode=mode)
-        self.genotypes=genotypes
         self.init_group_structure()
+        self.group_mfest = ['RAW', 'PROC', 'LABEL']
+        self.hormones = ['control', 'ga', 'pac', 'pac+ga', 'ucn', 'pcz']
+        self.genos = ['B73', 'Cml103', 'Lh132', 'Mo17', 'Mo017', 'Mo066',
+                      'Mo276', 'Mo287', 'Mo352', 'Mo360', 'Oh7b', 'Oh43']
 
     def init_group_structure(self):
         hf = self.get_hf()
-        for geno in self.get_genos():
-            self.write_group(geno)
+        groups = [key for key in hf.keys()]
+
+        for group in self.group_mfest:
+            if group not in groups:
+                hf.create_group(group)
+
+    def get_group_mfest(self):
+        return self.group_mfest
+
+    def add_group_mfest(self, group):
+        self.group_mfest.append(group)
 
     def get_genos(self):
-        return self.genotypes
+        return self.genos
+
+    def get_hormones(self):
+        return self.hormones
+
+    def get_raws(self):  # , geno=None, hormone=None, packet=None):
+        """Returns 4D image cube of hsi's with list of image name identifiers"""
+        hf = self.get_hf()
+        # if geno is None:
+        raws = [raw for raw in hf['RAW'].keys()]
+        # elif geno is type(list):
+        #     raws = [raw for raw in hf['RAW'].keys() if any(gene in raw for gene in geno)]
+        # elif geno is type(str):
+        #     raws = [raw for raw in hf['RAW'].keys() if geno in raw]
+        # else:
+        #     raws = []
+        #     print('WARNING! No images selected')
+
+        images = [hf['RAW'][image] for image in raws]
+        return image_cube(images), raws
+
+
+def image_cube(images):
+    return np.stack(images, axis=-1)
 
 
 def list_dataset(name, node):
-    dsets = []
     if isinstance(node, h5.Dataset):
-        dsets.append(name)
+        print(name)
 
 
 def list_group(name, node):
-    groups = []
     if isinstance(node, h5.Group):
-        groups.append(name)
+        print(name)
 
 
 # pass as list of tuples to make 1 for loop generating file
@@ -198,38 +234,49 @@ def convert_bil_h5(file_path, img_paths, geno, store_metadata=False):
     :return:
     """
     hf = h5.File(file_path, 'a')
+    raw = hf.create_group("RAW")
+    img_tup = []
 
     for gene in geno:
-        if gene not in hf.keys():
-            group = hf.create_group(gene)
-        else:
-            group = hf[gene]
+        # if gene not in hf.keys():
+        #     group = hf.create_group(gene)
+        # else:
+        #     group = hf[gene]
 
-        sub_img_paths = [path for path in img_paths if gene in path]
+        sub_img_paths = [(gene,path) for path in img_paths if gene in path]
+        img_tup.extend(sub_img_paths)
 
-        for file in sub_img_paths:
-            f = os.path.basename(file)
-            packet, hormone, ext = f.split(".")
 
-            if hormone not in group.keys():
-                horm = group.create_group(hormone)
-                pac = horm.create_group(packet)
-            else:
-                horm = group[hormone]
-                if packet not in horm.keys():
-                    pac = horm.create_group(packet)
-                else:
-                    pac = horm[packet]
+    for file in img_tup:
+        f = os.path.basename(file[1])
+        name = f[:-4]
+        name = file[0] + "." + name
 
-            img_paths.remove(file)
-            img = open_hsi_bil(file)
-            name_idx = file.find(gene) + len(gene+'/')
-            cut_idx = file.find(".bil")
-            dset_name = file[name_idx:cut_idx]
-            # if dset_name not in pac.keys():
-            pac.create_dataset(dset_name, data=img, chunks=True)
-            if store_metadata:
-                write_metadata(pac, file)
+        #TODO: simplify group structure?
+
+        # if hormone not in group.keys():
+        #     horm = group.create_group(hormone)
+        #     pac = horm.create_group(packet)
+        # else:
+        #     horm = group[hormone]
+        #     if packet not in horm.keys():
+        #         pac = horm.create_group(packet)
+        #     else:
+        #         pac = horm[packet]
+
+        img_tup.remove(file)
+        img = open_hsi_bil(file[1])
+        # name_idx = file.find(gene) + len(gene+'/')
+        # cut_idx = file.find(".bil")
+        #TODO: removing packet number from dset name?
+
+        # dset_name = file[name_idx:cut_idx]
+        # if dset_name not in pac.keys():
+        print("writing data for %s" % name)
+        raw.create_dataset(name, data=img, chunks=True)
+        print("done")
+        # if store_metadata:
+        #     write_metadata(pac, file)
 
     hf.close()
 
