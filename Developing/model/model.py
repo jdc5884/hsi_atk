2b42640cf6bac1d4009df6ef216f134cafe4437c
David Ruddell <dlruddell@gmail.com>
2019-04-01 18:29:43 -0400

    Setting up model builder

--- Developing/model/model.py
+++ Developing/model/model.py
@@ -1,2 +1,19 @@
-# author David Ruddell
-# contact: dr1236@uncw.edu, dlruddell@gmail.com
+__author__ = "David Ruddell"
+__credits__ = ["David Ruddell"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__status__ = "Development"
+
+
+import tensorflow as tf
+
+
+class Model(tf.keras.Model):
+
+    def __init__(self):
+        super(Model, self).__init__()
+
+        self.build_model()
+
+    def build_model(self):
+        pass
