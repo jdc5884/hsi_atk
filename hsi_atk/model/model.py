c438120efc4489140427f825334c837ad77e96b4
David Ruddell <dlruddell@gmail.com>
2019-04-05 17:30:07 -0400

    This is written for tensorflow 2.0

--- hsi_atk/model/model.py
+++ hsi_atk/model/model.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 
 
-class Model(tf.keras.Model):
-
-    def __init__(self):
-        super(Model, self).__init__()
-
-        self.build_model()
-
-    def build_model(self):
-        pass
+# class Model(tf.keras.Model):
+#
+#     def __init__(self):
+#         super(Model, self).__init__()
+#
+#         self.build_model()
+#
+#     def build_model(self):
+#         pass
